aa729628241d942149164c0876aeb73a99ae61f5
Mesolith <peter.ditchfield@rlaha.ox.ac.uk>
2022-03-08 20:41:24 +0000

    PAT use instructions

---
+++ PAT tokens in R.R
@@ -0,0 +1,8 @@
+library("usethis")
+git_sitrep() # git situation report
+
+
+## if you have no token set
+## then create the token for the repository you want to work with on git hub remember to copy it from the page where it is created
+
+# then you can set it using the in the git situation report.
